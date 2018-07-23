361345e0c3f76cb51093e63ea59c0671d1176e7c
Oscar Sanchez <osanchez@extron.com>
2018-07-23 15:09:38 -0700

    Update Files

--- NBA Draft Analysis/test.py
+++ NBA Draft Analysis/test.py
@@ -5,92 +5,163 @@ Created on Fri Jul 13 14:39:36 2018
 @author: osanchez
 """
 
-seasons = pd.read_csv('Seasons_Stats.csv')
-players = pd.read_csv('player_data.csv')
-players.columns = ['Player', 'Rookie Year', 'Final Year', 'Position', 'Height','Weight','DOB','College']
+
+
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+seasons = pd.read_csv('C:/Users/osanchez/Downloads/Seasons_Stats.csv')
+
+
 
 
 years = list(range(1980,2018))
-years_draft = [1985, 1997, 2004]
-
 seasons = seasons[seasons['Year'].isin(years)]
-seasons = seasons[seasons['MP'] > 500]
-players = players[players['Rookie Year'].isin(years_draft)]
+
+
+
+
+seasons['MPG'] = seasons['MP'] / seasons['G']
+
+seasons = seasons[seasons['MPG'] >= 20]
+
+
+
+
+
+seasons[(seasons['Player'] == 'J.J. Redick') & (seasons['Year'] > 2011)]
+
+
+
 seasons = seasons[seasons['Tm'] != 'TOT']
+seasons['Player'] = seasons['Player'].str.replace('*','') ### Remove special character
 
 
 
 
-data = seasons[['Year', 'Player','G','MP','PER', 'TS%', 'FG%', '3P%','2P%', 'eFG%','OWS', 'DWS', 'WS', 'WS/48','USG%','OBPM', 'DBPM', 'BPM', 'VORP']]
+
+players = pd.read_csv('C:/Users/osanchez/Downloads/player_data.csv')
+years_draft = list(range(2010,2017))
+players.columns = ['Player', 'Rookie Year', 'Final Year', 'Position', 'Height','Weight','DOB','College']
+players = players[players['Rookie Year'].isin(years_draft)]
+players['Player'] = players['Player'].str.replace('*','') ### Remove special character
+
+
+
+
+
+
+
+data = seasons[['Year', 'Player','G','MP','MPG','PER', 'TS%', 'FG%', '3P%','2P%', 'eFG%','OWS', 'DWS', 'WS', 'WS/48',
+                'USG%','OBPM', 'DBPM', 'BPM', 'VORP']]
 data = data[data['Year'].isin(years)]
 data['Year'] = data['Year'].astype(object)
-data['Player'] = data['Player'].str.replace('*','')
 
 data_draft = players.merge(data, on = 'Player')
+data_draft['Rookie Year'] = data_draft['Rookie Year'] - 1 ### Set the Year to the correct draft class
 
-
-
-f = {'G': ['sum'],'MP': ['sum'],'PER': ['mean'], 'TS%': ['mean'], 'FG%': ['mean'], '3P%': ['mean'],'2P%': ['mean'], 'eFG%': ['mean'],'OWS': ['mean'], 'DWS': ['mean'], 'WS': ['mean'], 
-     'WS/48': ['mean'],'USG%': ['mean'],'OBPM': ['mean'], 'DBPM': ['mean'], 'BPM': ['mean'], 'VORP': ['mean']}
-
-
-data_1984 = data_1984.groupby(['Draft'], as_index = False).agg(f)
-data_1996 = data_1996.groupby(['Draft'], as_index = False).agg(f)
-data_2003 = data_2003.groupby(['Draft'], as_index = False).agg(f)
-
-combine = [data_1984, data_1996, data_2003]
-
-
-
-
-
-
-
-for item in combine:
-    item['Player'] = item['Player'].str.replace('+','')
-    item['Player'] = item['Player'].str.replace('*','')
-    item['Player'] = item['Player'].str.replace('#','')
-    item['Player'] = item['Player'].str.replace('~','')
-    item['Player'] = item['Player'].str.replace('^','')
-    item['Player'] = item['Player'].str.replace('>','')
-    item['Player'] = item['Player'].str.replace('\d+','')
-    item['Player'] = item['Player'].str.replace('[','')
-    item['Player'] = item['Player'].str.replace(']','')
-    item['Player'] = item['Player'].str.replace('Hakeem Olajuwon›','Hakeem Olajuwon')
-    item['Player'] = item['Player'].str.strip()
-
-
-
-
-
-
-
-
-data_1984 = df1.merge(data, on = 'Player')
-data_1996 = df2.merge(data, on = 'Player')
-data_2003 = df3.merge(data, on = 'Player')
-
-
-data_1984 = data_1984.groupby(['Draft'], as_index = False).sum()
-
-f = {'G': ['sum'],'MP': ['sum'],'PER': ['mean'], 'TS%': ['mean'], 'FG%': ['mean'], '3P%': ['mean'],'2P%': ['mean'], 'eFG%': ['mean'],'OWS': ['mean'], 'DWS': ['mean'], 'WS': ['mean'], 
-     'WS/48': ['mean'],'USG%': ['mean'],'OBPM': ['mean'], 'DBPM': ['mean'], 'BPM': ['mean'], 'VORP': ['mean']}
-
-
+f = {'G': ['sum'],'MP': ['sum'],'MPG': ['mean'],'PER': ['mean'], 'TS%': ['mean'], 'FG%': ['mean'], '3P%': ['mean'],'2P%': ['mean'], 
+     'eFG%': ['mean'],'OWS': ['mean'], 'DWS': ['mean'], 'WS': ['mean'], 'WS/48': ['mean'],'USG%': ['mean'],
+     'OBPM': ['mean'], 'DBPM': ['mean'], 'BPM': ['mean'], 'VORP': ['mean']}
 data_draft = data_draft.groupby(['Rookie Year'], as_index = False).agg(f)
 
-combine = pd.concat(combine)
+
 
 
 
 per = data_draft.sort_values([('PER','mean')], ascending = False).reset_index()
 pos = list(range(len(per['PER','mean'])))
 
-
-fig, ax = plt.subplots()
-plt.bar(pos, per['PER','mean'], width = 0.75, alpha = 0.75, label = per['Rookie Year'])
+plt.style.use('fivethirtyeight')
+fig, ax = plt.subplots(figsize = (15,8))
+plt.bar(pos, per['PER','mean'], width = 0.75, alpha = 0.75, label = per['Rookie Year'], edgecolor = 'gray', linewidth = 2)
 for i in pos:
-    plt.text(pos[i], 0.5, s = per['Rookie Year'][i],ha='center', va='bottom', rotation = 'vertical', color = 'white')
+    plt.text(pos[i], 6, s = per['Rookie Year'][i],ha='center', va='bottom', rotation = 'vertical',color = 'orange', size = 50)
+plt.text(x = -0.68, y = 18, s = 'Total Career PER - 1984 Draft Class lead in efficiency',fontsize = 30, weight = 'bold', alpha = .75)
+plt.text(x = -0.68, y = 17, s = 'Total Career PER for all NBA players drafted in 1984, 1996, and 2003',fontsize = 19, alpha = .85)
+plt.text(x = -0.68, y = -1.5, s = 'National Basketball Association                                                                                       Source: Basketball-Reference.com ', fontsize = 17,color = '#f0f0f0', backgroundcolor = 'grey')
+plt.xticks([],[])
+ax.set_ylabel('PER', size = 25)
+
+
+
+
+
+
+
+vorp = data_draft.sort_values([('VORP','mean')], ascending = False).reset_index()
+pos1 = list(range(len(vorp['VORP','mean'])))
+
+plt.style.use('fivethirtyeight')
+fig, ax = plt.subplots(figsize = (15,8))
+plt.bar(pos1, vorp['VORP','mean'], width = 0.75, alpha = 0.75, label = vorp['Rookie Year'], edgecolor = 'gray', linewidth = 2)
+for i in pos1:
+    plt.text(pos1[i], .6, s = vorp['Rookie Year'][i],ha='center', va='bottom', color = 'orange', size = 50)
+plt.text(x = -0.68, y = 2.35, s = 'Total Career VORP - Jordan and friends keep dominating',fontsize = 30, weight = 'bold', alpha = .75)
+plt.text(x = -0.68, y = 2.2, s = 'Total Career VORP for all NBA players drafted in 1984, 1996, and 2003',fontsize = 19, alpha = .85)
+plt.text(x = -0.7, y = -.15, s = 'National Basketball Association                                                                                       Source: Basketball-Reference.com ', fontsize = 17,color = '#f0f0f0', backgroundcolor = 'grey')
+plt.xticks([],[])
+ax.set_ylabel('VORP', size = 25)
+
+
+
+
+
+ws = data_draft.sort_values([('WS/48','mean')], ascending = False).reset_index()
+pos = list(range(len(ws['WS/48','mean'])))
+
+plt.style.use('fivethirtyeight')
+
+fig, ax = plt.subplots(figsize = (15,8))
+plt.bar(pos, ws['WS/48','mean'], width = 0.75, alpha = 0.75, label = ws['Rookie Year'], edgecolor = 'gray', linewidth = 2)
+for i in pos:
+    plt.text(pos[i], 0.045, s = ws['Rookie Year'][i],ha='center', va='bottom', color = 'orange', size =50)
+
+plt.text(x = -0.7, y = 0.135, s = 'Total Career WS/48 - We get it. 1984 is the best draft class',fontsize = 30, weight = 'bold', alpha = .75)
+plt.text(x = -0.7, y = 0.1255, s = 'Total Career WS/48 for all NBA players drafted in 1984, 1996, and 2003',fontsize = 19, alpha = .85)
+plt.text(x = -0.71, y = -.012, s = 'National Basketball Association                                                                                       Source: Basketball-Reference.com ', fontsize = 17,color = '#f0f0f0', backgroundcolor = 'grey')
+plt.xticks([],[])
+ax.set_ylabel('WS/48', size = 25)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -8,5 +8,5 @@ Repository containing a series of projects as part of my data science portfolio.
 - ### [Baseball Analysis](https://github.com/osanchez2323/Portfolio/blob/master/Baseball%20Analysis/Baseball%20Analysis%20Notebook.ipynb)
 An in-depth analysis on whether MLB players during the Steroid Era (1990s) were significantly better than players during today's game. Data is from [Sean Lahman's baseball database](http://www.seanlahman.com/baseball-database.html).
 
-- ### [NBA Draft Analysis](https://github.com/osanchez2323/Portfolio/blob/master/NBA%20Draft%20Analysis/NBA%20Draft%20Analysis.ipynb)
-An analysis on the best NBA draft classes.
+- ### [GOAT Talk: The Best NBA Draft Class](https://github.com/osanchez2323/Portfolio/blob/master/NBA%20Draft%20Analysis/NBA%20Draft%20Analysis.ipynb)
+Are modern day basketball players better than players from the 1980s? 1990s? This analysis takes a look at some of the best NBA draft classes, and determines which draft produced the best players. Data used for this analysis is from [Basketball Reference](https://www.basketball-reference.com).
